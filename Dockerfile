6317f626f52b68c5c3c7c4904455d6b94e11b6bf
BBM <bogdanbontos105@gmail.com>
2026-03-26 16:07:30 +0000

    Init: Faza 0 - Aplicatia de baza si Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# 1. Fundation
+FROM python:3.11-slim
+
+# Setting the working directory
+WORKDIR /app
+
+# 2. Bringing and installing the libraries
+COPY req.txt .
+RUN pip install -r req.txt
+
+# 3. Bringing all files from the project (inclusiv app.py)
+COPY . .
+
+# 4. Security - creating a temporary user
+RUN useradd -m -s /bin/bash tester
+USER tester
+
+# 5. Start command
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+ 
+from flask import Flask, jsonify
+
+#App initialize
+app = Flask(__name__)
+
+#1. Main route (homepage)
+@app.route('/')
+def home():
+	return "<h1>Sall! BBM's DevSecOps </h1>"
+
+#2. Health check route (for_Docker)
+@app.route('/health')
+def health_check():
+	#Returning status in a JSON file
+	return jsonify({"status": "OK", "message": "Sistem is running smoothly"}), 200
+#3. App starting point 
+if __name__ == '__main__':
+	#host='0.0.0.0', mandatory in Docker
+	app.run(host='0.0.0.0', port=5000)
+
